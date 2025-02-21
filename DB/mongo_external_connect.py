5d48d6d668294fef4b96eb6e2fbd8630e511fc59
somsomi2 <dltlsdud122@naver.com>
2025-02-21 15:55:53 +0900

    test_userdata

--- DB/mongo_external_connect.py
+++ DB/mongo_external_connect.py
@@ -1,19 +1,20 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://192.168.5.13:27017/")
-db = client["mydatabase"]
-collection = db["users"]
+client = MongoClient("mongodb://192.168.5.19:27017/")
+db = client["filter_test"]
+collection = db["test"]
 
-# 데이터 삽입
-users = [
-    {"name": "Alice", "age": 28, "city": "Seoul"},
-    {"name": "Bob", "age": 32, "city": "Busan"},
-    {"name": "Charlie", "age": 25, "city": "Incheon"}
-]
-collection.insert_many(users)
-print("데이터 삽입 완료!")
+# # 데이터 삽입
+# users = [
+#     {"name": "Somi", "age": 211, "city": "Seoul"},
+#     {"name": "Shin", "age": 312, "city": "Busan"},
+#     {"name": "Charlie", "age": 251, "city": "Incheon"}
+# ]
+# collection.insert_many(users)
+# print("데이터 삽입 완료!")
 
 # 데이터 조회
 print("모든 데이터 조회:")
 for user in collection.find():
     print(user)
+
