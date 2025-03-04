5b4d6589957a95ecf43f49bf26ff1b87fa61b0db
Serin <serin1789@hanmail.net>
2025-03-04 16:42:55 +0900

    신영이대신 매치무브업뎃

--- open/step/open_layout.py
+++ open/step/open_layout.py
@@ -8,14 +8,50 @@ from step_open_maya import StepOpenMaya
 class LayoutStep(StepOpenMaya):
     def __init__(self):
         super().__init__()
-        print ("모델링 불러오기")
+        print ("layout")
 
     def open(self):
-        if not cmds.objExists("layout"):
-            cmds.group(em=True, name="layout")
-            print("The layout group was created.")
+        if not cmds.objExists("rig"):
+            cmds.group(em=True, name="rig")
+            print("The rig group was created.")
         else:
-            print("A layout group already exists.")
+            print("A rig group already exists.")
+
+        if not cmds.objExists("terrain"):
+            cmds.group(em=True, name="terrain")
+            print("The terrain group was created.")
+        else:
+            print("A terrain group already exists.")
+
+        if not cmds.objExists("camera"):
+            cmds.group(em=True, name="camera")
+            print("The camera group was created.")
+        else:
+            print("A camera group already exists.")
+            
+    def reference_rig(self):
+        rig_file =""
+        if os.path.exists(rig_file):
+            cmds.file(rig_file, name="rig")
+            print("The rig {rig_file} was referenced.")
+        else:
+            print("The rig {rig_file} was not found.")
+    
+    def reference_terrain(self):
+        terrain_file =""
+        if os.path.exists(terrain_file):
+            cmds.file(terrain_file, reference=True, namesapce="terrain")
+            print("The terrain {terrain_file} was referenced.")
+        else:
+            print("The terrain {terrain_file} was not found.")
+    
+    def reference_camera(self):
+        camera_file =""
+        if os.path.exists(camera_file):
+            cmds.file(camera_file, reference=True, namesapce="camera")
+            print("The camera {camera_file} was referenced.")
+        else:
+            print("The camera {camera_file} was not found.")
 
 
 layout = LayoutStep()
