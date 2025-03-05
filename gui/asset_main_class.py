5ff9cf434c8005cf4bc6c61cbc9da86a645044f1
nayeoncho0812 <0812okk@naver.com>
2025-03-05 09:17:21 +0900

    자 힘차는 하루 시작전 commit

--- gui/asset_main_class.py
+++ gui/asset_main_class.py
@@ -478,7 +478,7 @@ class MainUi(QMainWindow):
         self.ui.user_num.setText("b976211")
 
     def load_ui(self):
-        ui_file_path = "./asset_main2.ui"
+        ui_file_path = "/home/rapa/NA_Spirit/gui/asset_main2.ui"
         ui_file = QFile(ui_file_path)  
         loader = QUiLoader() 
         self.ui = loader.load(ui_file) 
