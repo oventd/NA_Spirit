44498568590280cf172fbe1fd5594a646386fddd
Serin <serin1789@hanmail.net>
2025-03-06 14:18:33 +0900

    이게 진짜임

--- open/step/open_modeling.py
+++ open/step/open_modeling.py
@@ -2,39 +2,46 @@ import maya.mel as mel
 import maya.cmds as cmds
 import os
 import sys
-sys.path.append(STEP_PATH)
+sys.path.append('/home/rapa/NA_Spirit/open/step')
 from step_open_maya import StepOpenMaya
-sys.path.append(UTILS_PATH)
-from constant import * 
+sys.path.append('/home/rapa/NA_Spirit/utils')
 from maya_utils import create_group, validate_hierarchy  # 유틸 함수 임포트
 
 class ModelingStep(StepOpenMaya):
     def __init__(self):
         super().__init__()
         print ("모델링 불러오기")
+        self.group_name = None
 
-    class Open:
-        @staticmethod
-        def setup(group_name=GEO):
-            group_name = group_name
-            create_group(group_name)
+    def open(self, group_name="geo"):
+        self.group_name = group_name
+        create_group(group_name)
+        
+        if not validate_hierarchy("geo", ["Low", "High"]):
+            print("Geo 그룹 하위에 Low와 High 그룹이 모두 존재하지 않습니다. 파일을 새로 생성합니다")
 
-    class Publish:
-        @staticmethod
-        def validate(group_name=GEO):
-            if validate_hierarchy(group_name=group_name):
-                print(f"Validation passed:'{group_name}' exists.")
-            else:
-                print(f"Validation failed:'{group_name}' does not exist.")
+            if not validate_hierarchy("geo"):
+                create_group("geo")
+            
+            create_group("Low", parent = "geo")
+            create_group("High", parent = "geo")
+            print("이제 Geo 그룹 하위에 Low와 High 그룹이 모두 존재합니다.")
 
-            if validate_hierarchy(GEO, ["Low", "High"]):
-                print("Geo 그룹 하위에 Low와 High 그룹이 모두 존재합니다.")
-            else:
-                print("Geo 그룹 하위에 Low와 High 그룹이 모두 존재하지 않습니다.")
+    def validate(self):
+        if validate_hierarchy(group_name=self.group_name):
+            print(f"Validation passed:'{self.group_name}' exists.")
+        else:
+            print(f"Validation failed:'{self.group_name}' does not exist.")
+
+
+        if validate_hierarchy("geo", ["Low", "High"]):
+            print("Geo 그룹 하위에 Low와 High 그룹이 모두 존재합니다.")
+        else:
+            print("Geo 그룹 하위에 Low와 High 그룹이 모두 존재하지 않습니다.")
 
 
 if __name__ == "__main__":
     modeling = ModelingStep()
-    ModelingStep.Open.setup(group_name=GEO)
-    ModelingStep.Publish.validate(group_name=GEO)
+    modeling.open()
+    modeling.validate()
     
