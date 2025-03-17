02f56359bab22641a413cfa3dea8592fad117aea
lllY <chochochuxx@gmail.com>
2025-03-17 10:28:51 +0900

    나연언니 브랜치 바꾸기전에 test.py 올려놔요

--- gui/test.py
+++ gui/test.py
@@ -1,8 +1,9 @@
-exm_list =[]
 
-exemples = [{"1":"apple"}, {"2":"banana"}, {"3":"cherry"},{"4":"cherry"},{"5":"cherry"},{"6":"cherry"}]
-for exemple in exemples:
-    for keys,values in exemple.items():
-        exm_list.append(values)
+asset_list=[{'_id': ObjectId('67d422060172b3b1c9c086be'), 
+  'asset_id': '753', 'name': 'Fine Wire Mesh', 
+  'description': 'A stretched, diamond-patterned metal mesh with high durability.', 'asset_type': 'Texture', 'category': 'Weapon', 'style': 'Realistic', 'resolution': '512x512', 'file_format': 'JPG', 'size': '243MB', 'license_type': 'Free', 'creator_id': '8220', 'creator_name': 'Liam Scott', 'downloads': 444, 'created_at': datetime.datetime(2024, 12, 27, 7, 18, 29, 240000), 'updated_at': datetime.datetime(2024, 12, 27, 7, 18, 29, 240000), 'preview_url': '/nas/spirit/DB/thum/texture/grill_texture_preview.png', 'image_url': ['/nas/spirit/DB/thum/texture/grill_texture_detail.png', '/nas/spirit/DB/thum/texture/grill_presetting_000.png', '/nas/spirit/DB/thum/texture/grill_presetting_001.png', '/nas/spirit/DB/thum/texture/grill_presetting_002.png'], 'source_url': None, 'video_url': None}]
 
-print(exm_list)
+asset_dict = {str(asset["_id"]): asset["name"] for asset in asset_list}
+print(asset_dict)
+        
+
