ad9f7f8245940ccda7ff90147c11812afecc193a
nayeoncho0812 <0812okk@naver.com>
2025-03-19 06:29:54 +0900

    상수 추가

--- utils/constant.py
+++ utils/constant.py
@@ -2,7 +2,7 @@
 Spirit 전체 상수 파일입니다.
 """
 # DB 커넥션 상수
-MONGODB_ADRESS = "mongodb://192.168.5.10:27017/" # mongoDB 주소 192.168.5.10 192.168.122.1 
+MONGODB_ADRESS = "mongodb://192.168.5.10:27017" # mongoDB 주소
 DATA_BASE = "spiritDatabase" # 접속할 데이터 베이스 명
 USER_COLLECTION = "test" # 데이터 베이스에서 접속할 컬렉션 명
 
@@ -27,6 +27,14 @@ UPDATED_AT = "updated_at"  # updated_at 값
 PRICE = "price"  # price 값
 PREVIEW_URL = "preview_url"  # preview_url 값
 
+SEARCH_FIELDS = ["_id", "name", "description", "asset_type", "category", 
+"style", "resolution", "file_format", "size", "license_type",
+"creator_id", "creator_name", "downloads", "price", 
+"image_url","source_url","video_url", "preview_url",
+"created_at", "updated_at"]
+
+
+# 버려도 되는 친구
 DETAIL_URL = "detail_url"  # particular_url 값
 PRESETTING_URL1 = "presetting_url1"  # presetting_url1 값
 PRESETTING_URL2 = "presetting_url2"  # presetting_url2 값
@@ -46,6 +54,14 @@ LOGGER_NAME = "db_crud" # DbCrud(객체 생성용) 로거 이름
 DB_LOGGER_DIR = "/nas/spirit/DB/db_logger" # 로거 저장 경로
 ASSET_LOGGER_NAME = "user_db" # 로거 저장 이름
 ASSET_LOGGER_DIR = "/nas/spirit/DB/log/asset_library.log" # 에셋 로거 저장 경로
+
+
+# ux logger 관련 상수
+UX_Like_ASSET_LOGGER_NAME = "ux_like_asset" # user_ux(객체 생성용) 로거 이름
+UX_Like_ASSET_LOGGER_DIR = "/nas/spirit/gui/log/like_asset_logger.log" # 로거 저장 이름
+
+UX_DOWNLOAD_LOGGER_NAME = "ux_download" # user_ux(객체 생성용) 로거 이름
+UX_DOWNLOAD_LOGGER_DIR = "/nas/spirit/gui/log/download_logger.log" # 로거 저장 이름
 
 # Shotgrid Pipeline Step 상수
 MODELING = 'Model'
@@ -82,19 +98,7 @@ ANM = 'ANM' # Animation
 LGT = 'LGT' # Light
 CMP = 'CMP' # Compositing
 
-
-#Shotgrid Project 상수
-SERVER_PATH = 'https://5thacademy.shotgrid.autodesk.com'
-SCRIPT_NAME = 'nayeon_key'
-API_KEY = 'h0mvmfnhuochunhzpgR~zlpur'
-
-
-#Shotgrid Project 상수
-SERVER_PATH = 'https://5thacademy.shotgrid.autodesk.com'
-SCRIPT_NAME = 'nayeon_key'
-API_KEY = 'h0mvmfnhuochunhzpgR~zlpur'
-
-STEP_SHORT_DICT = {
+shot_step_dict = {
     MODELING: MDL,
     RIGGING: RIG,
     LOOKDEV: LDV,
@@ -105,39 +109,7 @@ STEP_SHORT_DICT = {
     COMPOSITING: CMP
 }
 
-SHORT_STEP_DICT = {
-    MDL : MODELING,
-    RIG : RIGGING,
-    LDV : LOOKDEV,
-    MMV : MATCHMOVE,
-    LAY : LAYOUT,
-    ANM : ANIMATING,
-    LGT : LIGHTING,
-    CMP : COMPOSITING
-}
-#   "Rig": {
-#     "module": "open_rigging",
-#     "class": "RiggingStep",
-#     "path": "/home/rapa/NA_Spirit/open/step/open_rigging.py"
-#   }  ,
-#     },
-#   "Matchmove": {
-#     "module": "open_matchmove",
-#     "class": "MatchMoveStep",
-#     "path": "/home/rapa/NA_Spirit/open/step/open_matchmove.py"
-#   },
-#   "Layout": {
-#     "module": "open_layout",
-#     "class": "LayoutStep",
-#     "path": "/home/rapa/NA_Spirit/open/step/open_layout.py"
-#   },
-#   "Animation": {
-#     "module": "open_animating",
-#     "class": "AnimatingStep",
-#     "path": "/home/rapa/NA_Spirit/open/step/open_animating.py"
-#   },
-#   "Light": {
-#     "module": "open_lighting",
-#     "class": "LightingStep",
-#     "path": "/home/rapa/NA_Spirit/open/step/open_lighting.py"
-#   }  
+#Shotgrid Project 상수
+SERVER_PATH = 'https://5thacademy.shotgrid.autodesk.com'
+SCRIPT_NAME = 'nayeon_key'
+API_KEY = 'h0mvmfnhuochunhzpgR~zlpur'
