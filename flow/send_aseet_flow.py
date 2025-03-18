6c8a9556eebd1f8cfed93162dbe9d7aa0eba160e
nayeoncho0812 <0812okk@naver.com>
2025-03-18 11:16:22 +0900

    넘어오는 리스트 추가

---
+++ flow/send_aseet_flow.py
@@ -0,0 +1,5 @@
+#프린트 형식
+# ['67bd6ea2e63d06c897d0de1d', '67bd6ec57dbd058b9f0d9997', '67bd6ec57dbd058b9f0d999b']이 레퍼런스로 다운로드되었습니다
+# ['67bd6ea2e63d06c897d0de1d']에셋이 임포트로 다운되었습니다
+
+['67bd6ea2e63d06c897d0de1d', '67bd6ec57dbd058b9f0d9997', '67bd6ec57dbd058b9f0d999b']
