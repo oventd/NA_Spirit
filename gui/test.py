17d822fd1abc5e06bde87cfbe67080ffa65de7d0
lllY <chochochuxx@gmail.com>
2025-03-19 14:07:48 +0900

    파일 수정

--- gui/test.py
+++
@@ -1,8 +0,0 @@
-exm_list =[]
-
-exemples = [{"1":"apple"}, {"2":"banana"}, {"3":"cherry"},{"4":"cherry"},{"5":"cherry"},{"6":"cherry"}]
-for exemple in exemples:
-    for keys,values in exemple.items():
-        exm_list.append(values)
-
-print(exm_list)
