827913a7fa603d7da4836560cd0ba7f24f7cc33f
somsomi2 <165898290+somsomi2@users.noreply.github.com>
2025-03-20 12:11:05 +0900

    delate_db trashcan

--- DB/main.py
+++
@@ -1,8 +0,0 @@
-#from gui.ui_window import *  # main.py에서 gui.ui_window를 임포트하여 실행
-from gui.신영_테스트용 import *
-
-if __name__ == "__main__":
-    run_app()  # 애플리케이션 실행
-
-# a = CustomTableModel([])
-# print(f"!!!!!{a.value}")
